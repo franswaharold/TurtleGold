b0f79e174405700fefc286d057d94c6a6e22043b
Morpheus <43630833+19morpheus80@users.noreply.github.com>
2019-01-30 18:05:10 +0000

    added seeds
    +stroppy
    +sniper
    Should help things along

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -214,5 +214,7 @@ const char* const SEED_NODES[] = {
     "97.64.253.98:42069", // morpheus
     "51.255.209.200:42069", // explorer.dego.gq
     "23.96.93.180:42069" // Aluisyo
+    "116.250.211.215:42069", // stroppy
+    "5.172.219.172" //sniperviperman
 };
 } // CryptoNote
