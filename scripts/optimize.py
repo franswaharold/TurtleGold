44ad5fe1fb25eb58b22496f8b86b2a2b4c22b57f
Richard <37025447+MysticCraftYT@users.noreply.github.com>
2019-04-04 19:25:08 +0200

    Name fix.

--- scripts/optimize.py
+++ scripts/optimize.py
@@ -5,7 +5,7 @@ from turtlecoin import Walletd as Wallet
 import time
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument("--address", help="Turtlecoin address to optimize", required=True)
+parser.add_argument("--address", help="TurtleGold address to optimize", required=True)
 parser.add_argument("--port", type=int, help="Wallet port", required=False, default=8070)
 parser.add_argument("--password", help="Wallet RPC Password", required=True)
 parser.add_argument("--wait", type=int, help="Time to wait between fusions", required=False, default=5)
