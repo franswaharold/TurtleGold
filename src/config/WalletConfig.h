3f18204eb9c99e9a644156df5321a459fe5dd56c
Richard <37025447+MysticCraftYT@users.noreply.github.com>
2019-04-04 19:17:29 +0200

    fixed a thing..
    fixed a thing..

--- src/config/WalletConfig.h
+++ src/config/WalletConfig.h
@@ -1,5 +1,6 @@
 // Copyright (c) 2018, The TurtleCoin Developers
 // Copyright (c) 2018, The DeroGold Association
+// Copyright (c) 2019, The TurtleGold Developers
 // 
 // Please see the included LICENSE file for more information.
 
